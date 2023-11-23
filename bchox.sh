ff614cbcde2c9e834722a0262359dd14e88bff7b
bchox <151778760+bchox@users.noreply.github.com>
2023-11-23 09:59:14 +0100

    Update bchox.sh - it will look if folder already exists

--- bchox.sh
+++ bchox.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
 # Install yay
+
 sudo pacman -S --needed base-devel git
-mkdir ~/github
-cd github
+mkdir -p ~/github
+cd ~/github
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-cd ~/
+cd ..
 rm -rf ~/github/yay
 
 ##====INSTALL ALL YAY PACKAGES====##
@@ -19,32 +19,38 @@ install_packages() {
 }
 
 # Install the specified packages.
-install_packages extra/fish extra/kitty extra/ttf-sourcecodepro-nerd aur/powerline-shell aur/swww extra/firefox extra/waybar
+install_packages extra/fish extra/kitty extra/ttf-sourcecodepro-nerd aur/powerline-shell aur/swww extra/firefox
 
 ##====DONE INSTALLING PACKAGES====##
 
 # Download wallpapers in ~/wallpaper
+
 cd ~
 git clone https://gitlab.com/stephan-raabe/wallpaper.git
 
 # Set fish configuration
-mkdir ~/.config/fish
-cp .config/fish/config.fish ~/.config/fish/config.fish
+config_fish=~/.config/fish
+mkdir -p $config_fish
+cp .config/fish/config.fish $config_fish/config.fish
 
 # Set kitty configuration
-mkdir ~/.config/kitty
-cp .config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+config_kitty=~/.config/kitty
+mkdir -p $config_kitty
+cp .config/kitty/kitty.conf $config_kitty/kitty.conf
 
 # Set Powerline-shell configuration
-sudo cp .config/powerline-shell/__init__.py /usr/lib/python3.11/site-packages/powerline-shell/__init__.py
-mkdir -p ~/.config/powerline-shell/themes
-cp .config/powerline-shell/themes/flames.py ~/.config/powerline-shell/themes/flames.py
-cp .config/powerline-shell/config.json ~/.config/powerline-shell/config.json
+config_powerline=~/.config/powerline-shell
+mkdir -p $config_powerline/themes
+cp .config/powerline-shell/__init__.py /usr/lib/python3.11/site-packages/powerline-shell/__init__.py
+cp .config/powerline-shell/themes/flames.py $config_powerline/themes/flames.py
+cp .config/powerline-shell/config.json $config_powerline/config.json
 
 # Set SWWW Random script
-mkdir ~/.config/swww
-cp .config/swww/swwwran.sh ~/.config/swww/swwwran.sh
+config_swww=~/.config/swww
+mkdir -p $config_swww
+cp .config/swww/swwwran.sh $config_swww/swwwran.sh
 
 # Set Hyprland config file
-
-cp .config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+config_hypr=~/.config/hypr
+mkdir -p $config_hypr
+cp .config/hypr/hyprland.conf $config_hypr/hyprland.conf
