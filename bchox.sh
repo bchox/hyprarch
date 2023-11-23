4bf3c8f5aa0e2dd1f06de5389a8493d287a34c7e
bchox <bchocotoff@gmail.com>
2023-11-23 02:12:43 +0100

    hyprarch

--- bchox.sh
+++ bchox.sh
@@ -29,23 +29,22 @@ cd ~
 git clone https://gitlab.com/stephan-raabe/wallpaper.git
 
 # Set fish configuration
-
 mkdir ~/.config/fish
 cp .config/fish/config.fish ~/.config/fish/config.fish
 
 # Set kitty configuration
-
 mkdir ~/.config/kitty
 cp .config/kitty/kitty.conf ~/.config/kitty/kitty.conf
 
 # Set Powerline-shell configuration
-
 sudo cp .config/powerline-shell/__init__.py /usr/lib/python3.11/site-packages/powerline-shell/__init__.py
 mkdir -p ~/.config/powerline-shell/themes
 cp .config/powerline-shell/themes/flames.py ~/.config/powerline-shell/themes/flames.py
 cp .config/powerline-shell/config.json ~/.config/powerline-shell/config.json
 
-# Configure SWWW script
+# Set SWWW Random script
+mkdir ~/.config/swww
+cp .config/swww/swwwran.sh ~/.config/swww/swwwran.sh
 
 # Set Hyprland config file
 
